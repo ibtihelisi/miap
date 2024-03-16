0730127d76b1156439c05cb7c358820ef8dacb3d
ibtihel <114367327+ibtihelisi@users.noreply.github.com>
2024-03-16 01:57:44 +0100

    sixth commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,12 +80,80 @@
 
 
     <div id="app">
-         <!-- top bar start-->
+        
+         <!-- Navigation -->
+ <nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+    <div class="container">
 
-       @include('inc.guest.topbar')
-       <!-- top bar end-->
-       
+        <!-- Image Logo -->
+        <a class="navbar-brand logo-image" href="/"><img src="{{asset('mainassets/images/logo.svg')}}" alt="alternative"></a> 
 
+        <!-- Text Logo - Use this if you don't have a graphic logo -->
+        <!-- <a class="navbar-brand logo-text" href="index.html">MenuQR</a> -->
+
+        <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+            
+
+
+             <!-- Right Side Of Navbar -->
+             <ul class="navbar-nav ms-auto">
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="btn-outline-sm" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="btn-outline-sm" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+
+
+
+
+
+
+            <!--<span class="nav-item">
+                <a class="btn-outline-sm" href="log-in.html">Log in</a>
+            </span>-->
+            <!-- <span class="nav-item">
+                <a class="btn-outline-sm" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </span>
+
+            <span class="nav-item">
+                <a class="btn-outline-sm" href="{{ route('register') }}">{{ __('Sign up') }}</a>
+            </span>-->
+        </div> <!-- end of navbar-collapse -->
+    </div> <!-- end of container -->
+</nav> <!-- end of navbar -->
+<!-- end of navigation -->
         <main class="py-4">
             @yield('content')
         </main>
